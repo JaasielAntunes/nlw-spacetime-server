d10ac65b599efe5c87222a49c68197e2162f09d4
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-17 14:23:30 -0300

    Criando rota de listagem de memórias

---
+++ src/routes/memories.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get("/memories", async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        cover: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat("..."),
+      };
+    });
+  });
+
+  app.get("/memories/:id", async (request) => {});
+
+  app.post("/cadastrar", async () => {});
+
+  app.put("/atualizar", async () => {});
+}
