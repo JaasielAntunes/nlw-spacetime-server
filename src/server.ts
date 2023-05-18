6a3912698c76998c9018d786bcdd77c546cd23f7
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-17 22:12:53 -0300

    Adicionando validação usando token JWT

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -9,11 +12,17 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
     port: 3030,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3030')
